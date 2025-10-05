36dda0cee22b09ac6580e11b60b71268389e9617
jharedMc <88927873+jharedMc@users.noreply.github.com>
2025-10-05 14:39:36 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is online")
+
+bot.run(os.getenv("TOKEN"))
